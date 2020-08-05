2a66fc93592ed4576efb13cdad0b1391aa460a6e
Scott Leberknight <scottleber@gmail.com>
2020-08-05 14:10:16 -0400

    Add DropwizardAppTests
    Fixes #28

---
+++ src/main/java/org/kiwiproject/test/dropwizard/app/DropwizardAppTests.java
@@ -0,0 +1,407 @@
+package org.kiwiproject.test.dropwizard.app;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+
+import com.google.common.annotations.Beta;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Sets;
+import io.dropwizard.Configuration;
+import io.dropwizard.jersey.DropwizardResourceConfig;
+import io.dropwizard.jersey.setup.JerseyEnvironment;
+import io.dropwizard.lifecycle.JettyManaged;
+import io.dropwizard.lifecycle.Managed;
+import io.dropwizard.lifecycle.ServerLifecycleListener;
+import io.dropwizard.lifecycle.setup.LifecycleEnvironment;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import lombok.experimental.UtilityClass;
+import org.eclipse.jetty.util.component.LifeCycle;
+import org.glassfish.jersey.internal.inject.InstanceBinding;
+import org.kiwiproject.reflect.KiwiReflection;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+/**
+ * Test utility for testing Dropwizard apps when using {@link DropwizardAppExtension}.
+ */
+@UtilityClass
+public class DropwizardAppTests {
+
+    /**
+     * Dropwizard does not expose this as a public class, so we have to resort to reflection and hackery to get at
+     * this class. It is, unfortunately, the class used when you register a {@link ServerLifecycleListener} in
+     * a Dropwizard application, so we need to access it somehow.
+     */
+    @VisibleForTesting
+    static final String DROPWIZARD_PRIVATE_SERVER_LISTENER_CLASS_NAME =
+            "io.dropwizard.lifecycle.setup.LifecycleEnvironment$ServerListener";
+
+    // Resources
+
+    /**
+     * Find the resource classes registered in the given Dropwizard app.
+     *
+     * @param app the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param <C> the configuration type
+     * @return list containing registered resource types
+     */
+    public static <C extends Configuration> List<Class<?>> registeredResourceClassesOf(DropwizardAppExtension<C> app) {
+        return registeredResourceClassesOf(app.getEnvironment().jersey());
+    }
+
+    /**
+     * Find the resource classes registered in the given {@link JerseyEnvironment}.
+     *
+     * @param jersey the {@link JerseyEnvironment} associated with the Dropwizard app being tested
+     * @return list containing registered resource types
+     */
+    public static List<Class<?>> registeredResourceClassesOf(JerseyEnvironment jersey) {
+        return registeredResourceObjectsOf(jersey)
+                .stream()
+                .map(Object::getClass)
+                .collect(toList());
+    }
+
+    /**
+     * Find the resource objects registered in the given Dropwizard app.
+     *
+     * @param app the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param <C> the configuration type
+     * @return list containing registered resource objects
+     */
+    public static <C extends Configuration> Set<Object> registeredResourceObjectsOf(DropwizardAppExtension<C> app) {
+        return registeredResourceObjectsOf(app.getEnvironment().jersey());
+    }
+
+    /**
+     * Find the resource objects registered in the given {@link JerseyEnvironment}.
+     *
+     * @param jersey the {@link JerseyEnvironment} associated with the Dropwizard app being tested
+     * @return list containing registered resource objects
+     * @implNote Dropwizard 2.0 added one more layer of indirection for resource endpoint classes; they are now
+     * wrapper inside a {@link DropwizardResourceConfig.SpecificBinder} so this method needs to unwrap those and
+     * add the wrapped objects to the returned set. To accomplish that, it has to perform some nastiness with the
+     * {@link org.glassfish.jersey.internal.inject.Binding} returned by
+     * {@link DropwizardResourceConfig.SpecificBinder#getBindings()}, specifically it must cast to
+     * {@link InstanceBinding} to be able to get the "service" which is the actual object we want. This is
+     * <em>extremely brittle</em> and we're only leaving it like this because this is a test helper class. Note also
+     * that the set returned by this class contains more objects that is returned by
+     * {@link DropwizardResourceConfig#getInstances()} since we do not remove the wrapped classes.
+     */
+    public static Set<Object> registeredResourceObjectsOf(JerseyEnvironment jersey) {
+        var instances = jersey.getResourceConfig().getInstances();
+
+        var wrappedResourceObjects = instances.stream()
+                .filter(o -> o instanceof DropwizardResourceConfig.SpecificBinder)
+                .map(DropwizardResourceConfig.SpecificBinder.class::cast)
+                .flatMap(specificBinder -> specificBinder.getBindings().stream())
+                .filter(binding -> binding instanceof InstanceBinding)
+                .map(InstanceBinding.class::cast)
+                .map(InstanceBinding::getService)
+                .collect(toSet());
+
+        return Sets.union(instances, wrappedResourceObjects);
+    }
+
+    // Health checks
+
+    /**
+     * Find the health check names registered in the given Dropwizard app.
+     *
+     * @param app the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param <C> the configuration type
+     * @return a set containing the health check names
+     */
+    public static <C extends Configuration> SortedSet<String> healthCheckNamesOf(DropwizardAppExtension<C> app) {
+        return app.getEnvironment().healthChecks().getNames();
+    }
+
+    // Lifecycle & Managed
+
+    /**
+     * Finds the {@link Managed} objects registered in the given Dropwizard app.
+     *
+     * @param app the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param <C> the configuration type
+     * @return list of Managed objects
+     */
+    public static <C extends Configuration> List<Managed> managedObjectsOf(DropwizardAppExtension<C> app) {
+        return managedObjectsOf(app.getEnvironment().lifecycle());
+    }
+
+    /**
+     * Finds the {@link Managed} objects registered in the given {@link LifecycleEnvironment}.
+     *
+     * @param lifecycle the {@link LifecycleEnvironment} associated with the Dropwizard app being tested
+     * @return list of Managed objects
+     */
+    public static List<Managed> managedObjectsOf(LifecycleEnvironment lifecycle) {
+        return managedObjectStreamOf(lifecycle).collect(toList());
+    }
+
+    /**
+     * Finds {@link Managed} objects registered in the given Dropwizard app and having the given type.
+     *
+     * @param app  the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param type the type of object to find
+     * @param <C>  the configuration type
+     * @param <T>  the object type
+     * @return list of managed objects
+     */
+    public static <C extends Configuration, T> List<T> managedObjectsOfType(DropwizardAppExtension<C> app,
+                                                                            Class<T> type) {
+        return managedObjectsOfType(app.getEnvironment().lifecycle(), type);
+    }
+
+    /**
+     * Finds {@link Managed} objects registered in the given {@link LifecycleEnvironment} and having the given type.
+     *
+     * @param lifecycle the {@link LifecycleEnvironment} associated with the Dropwizard app being tested
+     * @param type      the type of object to find
+     * @param <T>       the object type
+     * @return list of managed objects of type T
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> managedObjectsOfType(LifecycleEnvironment lifecycle, Class<T> type) {
+        return (List<T>) managedObjectsOf(lifecycle, managed -> type.isAssignableFrom(managed.getClass()));
+    }
+
+    /**
+     * Finds {@link Managed} objects registered in the given Dropwizard app that match the given predicate.
+     *
+     * @param app       the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param predicate the predicate to match
+     * @param <C>       the configuration type
+     * @return list of Managed objects
+     */
+    public static <C extends Configuration> List<Managed> managedObjectsOf(DropwizardAppExtension<C> app,
+                                                                           Predicate<Managed> predicate) {
+        return managedObjectsOf(app.getEnvironment().lifecycle(), predicate);
+    }
+
+    /**
+     * Finds {@link Managed} objects registered in the given {@link LifecycleEnvironment} that match the
+     * given predicate.
+     *
+     * @param lifecycle the {@link LifecycleEnvironment} associated with the Dropwizard app being tested
+     * @param predicate the predicate to match
+     * @return list of Managed objects
+     */
+    public static List<Managed> managedObjectsOf(LifecycleEnvironment lifecycle, Predicate<Managed> predicate) {
+        return managedObjectStreamOf(lifecycle)
+                .filter(predicate)
+                .collect(toList());
+    }
+
+    /**
+     * Finds the first {@link Managed} object registered in the given Dropwizard app having the given type.
+     *
+     * @param app  the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param type the type of object to find
+     * @param <C>  the configuration type
+     * @param <T>  the object type
+     * @return optional that might contain the first Managed that is found
+     */
+    public static <C extends Configuration, T> Optional<T> firstManagedObjectOfType(DropwizardAppExtension<C> app,
+                                                                                    Class<T> type) {
+        return firstManagedObjectOfType(app.getEnvironment().lifecycle(), type);
+    }
+
+    /**
+     * Finds the first {@link Managed} object registered in the given {@link LifecycleEnvironment} having the
+     * given type.
+     *
+     * @param lifecycle the {@link LifecycleEnvironment} associated with the Dropwizard app being tested
+     * @param type      the type of object to find
+     * @param <T>       the object type
+     * @return optional that might contain the first Managed that is found
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Optional<T> firstManagedObjectOfType(LifecycleEnvironment lifecycle, Class<T> type) {
+        return (Optional<T>) firstManagedObject(lifecycle, managed -> type.isAssignableFrom(managed.getClass()));
+    }
+
+    /**
+     * Finds the first {@link Managed} object registered in the given Dropwizard app matching the given predicate.
+     *
+     * @param app       the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param predicate the predicate to match
+     * @param <C>       the configuration type
+     * @return optional that might contain the first Managed that is found
+     */
+    public static <C extends Configuration> Optional<Managed> firstManagedObject(DropwizardAppExtension<C> app,
+                                                                                 Predicate<Managed> predicate) {
+        return firstManagedObject(app.getEnvironment().lifecycle(), predicate);
+    }
+
+    /**
+     * Finds the first {@link Managed} object registered in the given {@link LifecycleEnvironment} matching the
+     * given predicate.
+     *
+     * @param lifecycle the {@link LifecycleEnvironment} associated with the Dropwizard app being tested
+     * @param predicate the predicate to match
+     * @return optional that might contain the first Managed that is found
+     */
+    public static Optional<Managed> firstManagedObject(LifecycleEnvironment lifecycle,
+                                                       Predicate<Managed> predicate) {
+        return managedObjectStreamOf(lifecycle)
+                .filter(predicate)
+                .findFirst();
+    }
+
+    /**
+     * Streams {@link Managed} objects registered in the given Dropwizard app.
+     *
+     * @param app the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param <C> the configuration type
+     * @return stream of Managed objects
+     */
+    public static <C extends Configuration> Stream<Managed> managedObjectStreamOf(DropwizardAppExtension<C> app) {
+        return managedObjectStreamOf(app.getEnvironment().lifecycle());
+    }
+
+    /**
+     * Streams {@link Managed} objects registered in the given {@link LifecycleEnvironment}.
+     *
+     * @param lifecycle the {@link LifecycleEnvironment} associated with the Dropwizard app being tested
+     * @return stream of Managed objects
+     */
+    public static Stream<Managed> managedObjectStreamOf(LifecycleEnvironment lifecycle) {
+        return lifeCycleStreamOf(lifecycle)
+                .map(JettyManaged.class::cast)
+                .map(JettyManaged::getManaged);
+    }
+
+    /**
+     * Finds {@link LifeCycle} objects registered in the given Dropwizard app.
+     *
+     * @param app the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param <C> the configuration type
+     * @return list of LifeCycle objects
+     */
+    public static <C extends Configuration> List<LifeCycle> lifeCycleObjectsOf(DropwizardAppExtension<C> app) {
+        return lifeCycleObjectsOf(app.getEnvironment().lifecycle());
+    }
+
+    /**
+     * Finds {@link LifeCycle} objects registered in the given {@link LifecycleEnvironment}.
+     *
+     * @param lifecycle the {@link LifecycleEnvironment} associated with the Dropwizard app being tested
+     * @return list of LifeCycle objects
+     */
+    public static List<LifeCycle> lifeCycleObjectsOf(LifecycleEnvironment lifecycle) {
+        return lifecycle.getManagedObjects();
+    }
+
+    /**
+     * Streams {@link LifeCycle} objects registered in the given Dropwizard app.
+     *
+     * @param app the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param <C> the configuration type
+     * @return stream of LifeCycle objects
+     */
+    public static <C extends Configuration> Stream<LifeCycle> lifeCycleStreamOf(DropwizardAppExtension<C> app) {
+        return lifeCycleStreamOf(app.getEnvironment().lifecycle());
+    }
+
+    /**
+     * Streams {@link LifeCycle} objects registered in the given {@link LifecycleEnvironment}.
+     *
+     * @param lifecycle the {@link LifecycleEnvironment} associated with the Dropwizard app being tested
+     * @return stream of LifeCycle objects
+     */
+    public static Stream<LifeCycle> lifeCycleStreamOf(LifecycleEnvironment lifecycle) {
+        return lifecycle.getManagedObjects().stream();
+    }
+
+    /**
+     * Finds {@link LifeCycle.Listener} classes registered with the given Dropwizard app.
+     *
+     * @param app the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param <C> the configuration type
+     * @return list of lifecycle listener classes
+     */
+    @Beta
+    public static <C extends Configuration> List<Class<?>> lifeCycleListenerClassesOf(
+            DropwizardAppExtension<C> app) {
+
+        return lifeCycleListenerClassesOf(app.getEnvironment().lifecycle());
+    }
+
+    /**
+     * Finds {@link LifeCycle.Listener} classes registered with the given {@link LifecycleEnvironment}.
+     *
+     * @param lifecycle the {@link LifecycleEnvironment} associated with the Dropwizard app being tested
+     * @return list of lifecycle listener classes
+     */
+    @Beta
+    public static List<Class<?>> lifeCycleListenerClassesOf(LifecycleEnvironment lifecycle) {
+        return lifeCycleListenersOf(lifecycle).stream()
+                .map(Object::getClass)
+                .collect(toList());
+    }
+
+    /**
+     * Finds {@link LifeCycle.Listener} objects registered with the given Dropwizard app.
+     *
+     * @param app the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param <C> the configuration type
+     * @return list of lifecycle listener objects
+     */
+    @Beta
+    public static <C extends Configuration> List<LifeCycle.Listener> lifeCycleListenersOf(
+            DropwizardAppExtension<C> app) {
+
+        return lifeCycleListenersOf(app.getEnvironment().lifecycle());
+    }
+
+    /**
+     * Finds {@link LifeCycle.Listener} objects registered with the given {@link LifecycleEnvironment}.
+     *
+     * @param lifecycle the {@link LifecycleEnvironment} associated with the Dropwizard app being tested
+     * @return list of lifecycle listener objects
+     */
+    @SuppressWarnings("unchecked")
+    @Beta
+    public static List<LifeCycle.Listener> lifeCycleListenersOf(LifecycleEnvironment lifecycle) {
+        return (List<LifeCycle.Listener>) KiwiReflection.getFieldValue(lifecycle, "lifecycleListeners");
+    }
+
+    /**
+     * Finds {@link ServerLifecycleListener} objects registered with the given Dropwizard app.
+     *
+     * @param app the DropwizardAppExtension containing the Dropwizard app being tested
+     * @param <C> the configuration type
+     * @return list of server lifecycle listener objects
+     */
+    @Beta
+    public static <C extends Configuration> List<ServerLifecycleListener> serverLifecycleListenersOf(
+            DropwizardAppExtension<C> app) {
+
+        return serverLifecycleListenersOf(app.getEnvironment().lifecycle());
+    }
+
+    /**
+     * Finds {@link ServerLifecycleListener} objects registered with the given {@link LifecycleEnvironment}.
+     *
+     * @param lifecycle the {@link LifecycleEnvironment} associated with the Dropwizard app being tested
+     * @return list of server lifecycle listener objects
+     */
+    @Beta
+    public static List<ServerLifecycleListener> serverLifecycleListenersOf(LifecycleEnvironment lifecycle) {
+        var serverListeners = lifeCycleListenersOf(lifecycle).stream()
+                .filter(listener ->
+                        listener.getClass().getName().equals(DROPWIZARD_PRIVATE_SERVER_LISTENER_CLASS_NAME))
+                .collect(toList());
+
+        return serverListeners.stream()
+                .map(listener -> KiwiReflection.getFieldValue(listener, "listener"))
+                .map(ServerLifecycleListener.class::cast)
+                .collect(toList());
+    }
+}
---
+++ src/test/java/org/kiwiproject/test/dropwizard/app/DropwizardAppTestsTest.java
@@ -0,0 +1,255 @@
+package org.kiwiproject.test.dropwizard.app;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.kiwiproject.collect.KiwiLists.first;
+
+import com.codahale.metrics.health.HealthCheck;
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.lifecycle.JettyManaged;
+import io.dropwizard.lifecycle.Managed;
+import io.dropwizard.lifecycle.ServerLifecycleListener;
+import io.dropwizard.setup.Environment;
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.component.AbstractLifeCycle;
+import org.eclipse.jetty.util.component.LifeCycle;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.util.List;
+
+@DisplayName("DropwizardAppTests")
+@ExtendWith(SoftAssertionsExtension.class)
+@ExtendWith(DropwizardExtensionsSupport.class)
+class DropwizardAppTestsTest {
+
+    @Getter
+    @Setter
+    static class Config extends Configuration {
+        private String tagLine;
+        private String answer;
+    }
+
+    @AllArgsConstructor
+    public static class TagLineResource {
+
+        private final String tagLine;
+
+        @GET
+        @Path("/tagline")
+        public String tagLine() {
+            return tagLine;
+        }
+    }
+
+    @AllArgsConstructor
+    public static class AnswerResource {
+
+        private final String answer;
+
+        @GET
+        @Path("/anyQuestion")
+        public String anyQuestion() {
+            return answer;
+        }
+    }
+
+    public static class TagLineHealthCheck extends HealthCheck {
+        @Override
+        protected Result check() {
+            return Result.healthy();
+        }
+    }
+
+    public static class AnswerHealthCheck extends HealthCheck {
+        @Override
+        protected Result check() {
+            return Result.healthy();
+        }
+    }
+
+    public static class Managed1 extends NoOpManaged {
+    }
+
+    public static class Managed2 extends NoOpManaged {
+    }
+
+    public static class UnmanagedManaged extends NoOpManaged {
+    }
+
+    public static class MyLifeCycleListener extends AbstractLifeCycle.AbstractLifeCycleListener {
+    }
+
+    public static class MyServerLifecycleListener implements ServerLifecycleListener {
+        @Override
+        public void serverStarted(Server server) {
+            // no-op
+        }
+    }
+
+    public static class App extends Application<Config> {
+        @Override
+        public void run(Config config, Environment environment) {
+            environment.jersey().register(new TagLineResource(config.getTagLine()));
+            environment.jersey().register(new AnswerResource(config.getAnswer()));
+
+            environment.healthChecks().register("tagLine", new TagLineHealthCheck());
+            environment.healthChecks().register("answer", new AnswerHealthCheck());
+
+            environment.lifecycle().manage(new Managed1());
+            environment.lifecycle().manage(new Managed2());
+
+            environment.lifecycle().addLifeCycleListener(new MyLifeCycleListener());
+            environment.lifecycle().addServerLifecycleListener(new MyServerLifecycleListener());
+        }
+    }
+
+    static final DropwizardAppExtension<Config> APP = new DropwizardAppExtension<>(App.class,
+            ResourceHelpers.resourceFilePath("dropwizard-app-unit-tests.yml"));
+
+    @Test
+    void shouldReturnRegisteredResourceClasses() {
+        assertThat(DropwizardAppTests.registeredResourceClassesOf(APP)).contains(
+                AnswerResource.class,
+                TagLineResource.class
+        );
+    }
+
+    @Test
+    void shouldReturnRegisteredResourceObjects() {
+        assertThat(DropwizardAppTests.registeredResourceObjectsOf(APP))
+                .hasAtLeastOneElementOfType(AnswerResource.class)
+                .hasAtLeastOneElementOfType(TagLineResource.class);
+    }
+
+    @Test
+    void shouldReturnHealthCheckNames() {
+        assertThat(DropwizardAppTests.healthCheckNamesOf(APP)).contains("answer", "tagLine");
+    }
+
+    @Test
+    void shouldReturnManagedObjects() {
+        assertThat(DropwizardAppTests.managedObjectsOf(APP))
+                .hasAtLeastOneElementOfType(Managed1.class)
+                .hasAtLeastOneElementOfType(Managed2.class);
+    }
+
+    @Test
+    void shouldReturnManagedObjectsUsingPredicate() {
+        assertThat(DropwizardAppTests.managedObjectsOf(APP, managed -> managed instanceof Managed2))
+                .hasAtLeastOneElementOfType(Managed2.class)
+                .doesNotHaveAnyElementsOfTypes(Managed1.class);
+    }
+
+    @Test
+    void shouldReturnStreamOfManagedObjects() {
+        assertThat(DropwizardAppTests.managedObjectStreamOf(APP))
+                .hasAtLeastOneElementOfType(Managed1.class)
+                .hasAtLeastOneElementOfType(Managed2.class);
+    }
+
+    @Test
+    void shouldReturnManagedObjectsOfSpecificType() {
+        var noOpManagedObjects = DropwizardAppTests.managedObjectsOfType(APP, NoOpManaged.class);
+
+        assertThat(noOpManagedObjects)
+                .hasAtLeastOneElementOfType(Managed1.class)
+                .hasAtLeastOneElementOfType(Managed2.class);
+    }
+
+    @Test
+    void shouldReturnFirstManagedOfType(SoftAssertions softly) {
+        var optionalManaged1 = DropwizardAppTests.firstManagedObjectOfType(APP, Managed1.class);
+        softly.assertThat(optionalManaged1).isPresent();
+
+        var optionalManaged2 = DropwizardAppTests.firstManagedObjectOfType(APP, Managed2.class);
+        softly.assertThat(optionalManaged2).isPresent();
+
+        var optionalManaged3 = DropwizardAppTests.firstManagedObjectOfType(APP, UnmanagedManaged.class);
+        softly.assertThat(optionalManaged3).isEmpty();
+    }
+
+    @Test
+    void shouldReturnFirstManagedObjectUsingPredicate() {
+        var optionalManaged = DropwizardAppTests.firstManagedObject(APP, this::isManaged1Or2);
+
+        assertThat(optionalManaged).isPresent();
+
+        var managed = optionalManaged.orElseThrow();
+        assertThat(managed).isInstanceOfAny(Managed1.class, Managed2.class);
+    }
+
+    private boolean isManaged1Or2(Managed managed) {
+        var managedClass = managed.getClass();
+        return Managed1.class.isAssignableFrom(managedClass) || Managed2.class.isAssignableFrom(managedClass);
+    }
+
+    @Test
+    void shouldReturnLifeCycleObjects() {
+        var lifeCycles = DropwizardAppTests.lifeCycleObjectsOf(APP);
+        assertLifeCycleObjects(lifeCycles);
+    }
+
+    @Test
+    void shouldReturnStreamOfLifeCycleObjects() {
+        var lifeCycles = DropwizardAppTests.lifeCycleStreamOf(APP).collect(toList());
+        assertLifeCycleObjects(lifeCycles);
+    }
+
+    private void assertLifeCycleObjects(List<LifeCycle> lifeCycles) {
+        assertThat(first(lifeCycles)).isInstanceOf(JettyManaged.class);
+
+        var mischiefManaged = lifeCycles.stream()
+                .map(JettyManaged.class::cast)
+                .map(JettyManaged::getManaged)
+                .filter(managed -> NoOpManaged.class.isAssignableFrom(managed.getClass()))
+                .map(NoOpManaged.class::cast)
+                .map(NoOpManaged::mischiefManaged)
+                .collect(toSet());
+
+        assertThat(mischiefManaged).containsOnly(true);
+    }
+
+    @Test
+    void shouldReturnLifeCycleListenerClasses(SoftAssertions softly) {
+        var classes = DropwizardAppTests.lifeCycleListenerClassesOf(APP);
+
+        softly.assertThat(classes).contains(MyLifeCycleListener.class);
+        softly.assertThat(classes).extracting(Class::getName)
+                .contains(DropwizardAppTests.DROPWIZARD_PRIVATE_SERVER_LISTENER_CLASS_NAME);
+    }
+
+    @Test
+    void shouldReturnLifeCycleListenerObjects(SoftAssertions softly) {
+        var listeners = DropwizardAppTests.lifeCycleListenersOf(APP);
+
+        softly.assertThat(listeners).hasAtLeastOneElementOfType(MyLifeCycleListener.class);
+
+        var classNames = listeners.stream()
+                .map(Object::getClass)
+                .map(Class::getName)
+                .collect(toList());
+
+        softly.assertThat(classNames).contains(DropwizardAppTests.DROPWIZARD_PRIVATE_SERVER_LISTENER_CLASS_NAME);
+    }
+
+    @Test
+    void shouldReturnServerLifecycleListeners() {
+        var serverLifecycleListeners = DropwizardAppTests.serverLifecycleListenersOf(APP);
+
+        assertThat(serverLifecycleListeners).hasAtLeastOneElementOfType(MyServerLifecycleListener.class);
+    }
+}
