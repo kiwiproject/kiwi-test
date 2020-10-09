ac6062654eab094a80e239b65e2e737af8299631
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-10-09 06:35:44 -0400

    Add MongoTestProperties (#125)
    * Add MongoTestProperties
    
    Fixes #124
    
    * Code cleanup
    
    * In unitTestDatabaseName(), change the checkState to a verify
      since at this point, the dbName should *always* be less than 64
      characters long. If it isn't then we made a mistake.
    * Remove TRACE logging from MongoTestProperties. I did not mean to
      leave that in the code.
    * Add test covering the case when there are multiple MongoDB hosts
    
    * More code cleanup and remove invalid database name characters
    
    * Replace any characters disallowed in MongoDB database names
    * Extract verification of DB name length to verifyDatabaseNameLength
      static method so it can be tested easily. Since we never expect it
      to happen we could not verify its actual behavior previously.
    * Add tests for invalid characters and verifying DB name length
    
    * Add support for stripping (default) or keeping domains in service hosts
    
    Should the domain be stripped from the host name, e.g. make
    service1.acme.com and service1.test be simply service1?

---
+++ src/main/java/org/kiwiproject/test/mongo/MongoTestProperties.java
@@ -0,0 +1,203 @@
+package org.kiwiproject.test.mongo;
+
+import static com.google.common.base.Verify.verify;
+import static java.util.Objects.isNull;
+import static java.util.stream.Collectors.joining;
+import static org.apache.commons.lang3.StringUtils.containsAny;
+import static org.apache.commons.lang3.StringUtils.replaceChars;
+import static org.kiwiproject.base.KiwiStrings.f;
+import static org.kiwiproject.base.KiwiStrings.splitOnCommas;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import lombok.Builder;
+import lombok.Value;
+import org.kiwiproject.net.KiwiUrls;
+
+import javax.annotation.Nullable;
+
+/**
+ * Simple value class that contains properties related to connecting to Mongo in the context of a service that uses
+ * MongoDB for persistence. The constructor and builder accept a service/application name and host, which helps to
+ * build easily identifiable and unique database names.
+ * <p>
+ * For example, if you are testing an "order-service" on your continuous integration server that resides on
+ * {@code ci-1.acme.com}, the test database name will include this information which can be useful debugging errors as
+ * well as to ensure multiple tests of the same service/application can run concurrently on different computers. The
+ * current timestamp is also included in the generated test database names to provide additional uniqueness in addition
+ * to the service/application name and host.
+ * <p>
+ * Intended to be used in conjunction with (TODO - add link reference to MongoDbExtension) though there is no reason it cannot be
+ * used standalone to generate database names for test purposes as well as the MongoDB client URI.
+ * <p>
+ * Per MongoDB <a href="https://docs.mongodb.com/manual/reference/limits/#naming-restrictions">Naming Restrictions</a>,
+ * "Database names cannot be empty and must have fewer than 64 characters". Thus the maximum length of database names
+ * is 63 characters. The generated test database names include the service/application and host name as well as either
+ * the {@link #UNIT_TEST_ID} (by default) or {@link #UNIT_TEST_ID_SHORT} (when the name needs to be truncated). The name
+ * always ends with the current time in millis. If the service/application name and host are long and including them
+ * in their entirety would cause the generated test name to exceed the Mongo name length limit, then various techniques
+ * to shorten the name are tried until the name is 63 characters or less.
+ */
+@Value
+public class MongoTestProperties {
+
+    /**
+     * The default ID used in generated database names to indicate the purpose is for testing.
+     */
+    public static final String UNIT_TEST_ID = "_unit_test_";
+
+    /**
+     * The short ID used in generated database names to indicate the purpose is for testing. Used in cases where
+     * the service/application name and host are lengthy and cause the default generated name to exceed the Mongo
+     * name length limit.
+     */
+    public static final String UNIT_TEST_ID_SHORT = "_ut_";
+
+    private static final String DEFAULT_DB_NAME_TEMPLATE = "{}" + UNIT_TEST_ID + "{}_{}";
+    private static final String DB_NAME_TEMPLATE = "{}" + UNIT_TEST_ID + "{}";
+    private static final String DB_SHORT_NAME_TEMPLATE = "{}" + UNIT_TEST_ID_SHORT + "{}";
+
+    // Invalid characters for MongoDB database names (union of invalid characters for Windows and Linux/Unix)
+    // Linux/Unix invalid characters: /\. "$
+    // Windows invalid characters:    /\. "$*<>:|?
+    // See https://docs.mongodb.com/manual/reference/limits/#naming-restrictions
+    private static final String INVALID_DB_NAME_CHARS = "/\\. \"$*<>:|?";
+
+    String hostName;
+    HostDomainBehavior hostDomainBehavior;
+    int port;
+    String serviceName;
+    String serviceHost;
+    String databaseName;
+    String uri;
+
+    /**
+     * Should the domain be kept or stripped in service host names?
+     */
+    public enum HostDomainBehavior {
+        /**
+         * Keep the domain name, e.g. {@code service1.acme.com} stays as-is.
+         */
+        KEEP,
+
+        /**
+         * Strip the domain name, e.g. {@code service1.acme.com} and {@code service1.test} both become {@code service1}
+         */
+        STRIP
+    }
+
+    /**
+     * Constructs a new instance.
+     * <p>
+     * Use the fluent builder as an alternative to this constructor.
+     *
+     * @param hostName           the host where MongoDB is located
+     * @param hostDomainBehavior how to handle domains in the given {@code hostName}
+     *                           (defaults to {@link HostDomainBehavior#STRIP STRIP} if this argument is {@code null})
+     * @param port               the port that MongoDB is listening on
+     * @param serviceName        the name of the service/application being tested
+     * @param serviceHost        the host of the service/application being tested
+     */
+    @Builder
+    public MongoTestProperties(String hostName,
+                               @Nullable HostDomainBehavior hostDomainBehavior,
+                               int port,
+                               String serviceName,
+                               String serviceHost) {
+        this.hostName = hostName;
+        this.hostDomainBehavior = isNull(hostDomainBehavior) ? HostDomainBehavior.STRIP : hostDomainBehavior;
+        this.port = port;
+        this.serviceName = serviceName;
+        this.serviceHost = serviceHost(serviceHost, hostDomainBehavior);
+        this.databaseName = unitTestDatabaseName(serviceName, serviceHost);
+        this.uri = mongoUri(hostName, port, databaseName);
+    }
+
+    private static String serviceHost(String serviceHost, HostDomainBehavior hostDomainBehavior) {
+        if (hostDomainBehavior == HostDomainBehavior.KEEP) {
+            return serviceHost;
+        }
+
+        return KiwiUrls.extractSubDomainNameFrom(serviceHost).orElseThrow();
+    }
+
+    @VisibleForTesting
+    static String unitTestDatabaseName(String serviceName, String serviceHost) {
+        var now = System.currentTimeMillis();
+        var dbName = f(DEFAULT_DB_NAME_TEMPLATE, serviceName, serviceHost, now);
+
+        // If name exceeds the limit of 63 characters, try removing the subdomain from hostname
+        if (nameExceedsMongoLength(dbName)) {
+            var subDomain = KiwiUrls.extractSubDomainNameFrom(serviceHost).orElse("");
+            dbName = f(DEFAULT_DB_NAME_TEMPLATE, serviceName, subDomain, now);
+        }
+
+        // If name still exceeds the limit, try removing the entire hostname
+        if (nameExceedsMongoLength(dbName)) {
+            dbName = f(DB_NAME_TEMPLATE, serviceName, now);
+        }
+
+        // If name still exceeds limit, try using the short ID
+        if (nameExceedsMongoLength(dbName)) {
+            dbName = f(DB_SHORT_NAME_TEMPLATE, serviceName, now);
+        }
+
+        // If name still exceeds limit, chop service name down to 46 characters. This also assumes the short test ID
+        // is 4 characters long, such that the total length is (46 + 4 + 13) = 60
+        // NOTE: 46 characters is accurate only so long as System.currentTimeMillis() returns a number 13 digits long,
+        // which means we're good through Sat Nov 20 2286 17:46:39 GMT+0000 (GMT)
+        if (nameExceedsMongoLength(dbName)) {
+            dbName = f(DB_SHORT_NAME_TEMPLATE, serviceName.substring(0, 46), now);
+        }
+
+        dbName = replaceInvalidDatabaseNameCharactersIfPresent(dbName);
+
+        verifyDatabaseNameLength(dbName);
+
+        return dbName;
+    }
+
+    private static String replaceInvalidDatabaseNameCharactersIfPresent(String dbName) {
+        if (containsAny(dbName, INVALID_DB_NAME_CHARS)) {
+            // Replace any invalid character with an underscore
+            return replaceChars(dbName, INVALID_DB_NAME_CHARS, "____________");
+        }
+
+        return dbName;
+    }
+
+    @VisibleForTesting
+    static void verifyDatabaseNameLength(String dbName) {
+        verify(!nameExceedsMongoLength(dbName),
+                "Unexpected error: DB name must be less than 64 characters in length, but was %s: %s",
+                dbName.length(), dbName);
+    }
+
+    private static boolean nameExceedsMongoLength(String dbName) {
+        return dbName.length() >= 64;
+    }
+
+    private static String mongoUri(String dbHostName, int dbPort, String databaseName) {
+        var hostAndPort = splitOnCommas(dbHostName)
+                .stream()
+                .map(hostname -> f("{}:{}", hostname, dbPort))
+                .collect(joining(","));
+        var optionalReplicaSet = dbHostName.contains(",") ? "?replicaSet=rs0" : "";
+
+        return f("mongodb://{}/{}{}",
+                hostAndPort,
+                databaseName,
+                optionalReplicaSet);
+    }
+
+    /**
+     * Create a new Mongo client for the test database described by the properties in this instance.
+     *
+     * @return a new {@link MongoClient}
+     */
+    public MongoClient newMongoClient() {
+        var mongoClientURI = new MongoClientURI(uri);
+        return new MongoClient(mongoClientURI);
+    }
+}
---
+++ src/test/java/org/kiwiproject/test/mongo/MongoTestPropertiesTest.java
@@ -0,0 +1,273 @@
+package org.kiwiproject.test.mongo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.kiwiproject.test.mongo.MongoTestProperties.unitTestDatabaseName;
+import static org.kiwiproject.test.mongo.MongoTestPropertiesTest.DatabaseName.assertDatabaseName;
+
+import com.google.common.base.VerifyException;
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.kiwiproject.test.mongo.MongoTestProperties.HostDomainBehavior;
+
+import java.util.regex.Pattern;
+
+@DisplayName("MongoTestProperties")
+@ExtendWith(SoftAssertionsExtension.class)
+class MongoTestPropertiesTest {
+
+    private static final String SERVICE_NAME = "sample-service";
+    private static final String FOURTEEN_CHAR_HOST_NAME = "sample-hostname";
+    private static final String FOURTEEN_CHAR_HOST_NAME_PLUS_DOMAINS = "sample-hostname.acme.com";
+    private static final String FIFTEEN_CHAR_SERVICE_NAME = "123456789012345";
+    private static final String TWENTY_THREE_CHAR_SERVICE_NAME = "12345678901234567890123";
+    private static final String TWENTY_FOUR_CHAR_SERVICE_NAME = "123456789012345678901234";
+    private static final String THIRTY_NINE_CHAR_SERVICE_NAME = "123456789012345678901234567890123456789";
+    private static final String FORTY_CHAR_SERVICE_NAME = "1234567890123456789012345678901234567890";
+    private static final String FORTY_SIX_CHAR_SERVICE_NAME = "1234567890123456789012345678901234567890123456";
+    private static final String FORTY_SEVEN_CHAR_SERVICE_NAME = "12345678901234567890123456789012345678901234567";
+    private static final String SIXTY_CHAR_SERVICE_NAME = "123456789012345678901234567890123456789012345678901234567890";
+    private static final Pattern ENDS_WITH_TIMESTAMP_PATTERN = Pattern.compile(".*_[0-9]{13,}$");
+
+    @Nested
+    class UnitTestDatabaseName {
+
+        @Test
+        void shouldIncludeServiceHost_givenNormalLookingNames() {
+            assertDatabaseName(unitTestDatabaseName(SERVICE_NAME, FOURTEEN_CHAR_HOST_NAME))
+                    .matchesPattern(SERVICE_NAME + "_unit_test_" + FOURTEEN_CHAR_HOST_NAME + "_")
+                    .withExpectedSize(54);
+        }
+
+        @Test
+        void shouldIncludeServiceHostSubDomainOnly_givenTwentyThreeCharacterServiceName_andFullHostName() {
+            assertDatabaseName(unitTestDatabaseName(FIFTEEN_CHAR_SERVICE_NAME, FOURTEEN_CHAR_HOST_NAME_PLUS_DOMAINS))
+                    .matchesPattern(FIFTEEN_CHAR_SERVICE_NAME + "_unit_test_" + FOURTEEN_CHAR_HOST_NAME + "_")
+                    .withExpectedSize(55);
+        }
+
+        @Test
+        void shouldIncludeServiceHost_givenTwentyThreeCharacterServiceName() {
+            assertDatabaseName(unitTestDatabaseName(TWENTY_THREE_CHAR_SERVICE_NAME, FOURTEEN_CHAR_HOST_NAME))
+                    .matchesPattern(TWENTY_THREE_CHAR_SERVICE_NAME + "_unit_test_" + FOURTEEN_CHAR_HOST_NAME + "_")
+                    .withExpectedSize(63);
+        }
+
+        @Test
+        void shouldNotIncludeServiceHost_givenTwentyFourCharacterServiceName() {
+            assertDatabaseName(unitTestDatabaseName(TWENTY_FOUR_CHAR_SERVICE_NAME, FOURTEEN_CHAR_HOST_NAME))
+                    .matchesPattern(TWENTY_FOUR_CHAR_SERVICE_NAME + "_unit_test_")
+                    .withExpectedSize(48);
+        }
+
+        @Test
+        void shouldNotIncludeServiceHost_givenThirtyNineCharacterServiceName() {
+            assertDatabaseName(unitTestDatabaseName(THIRTY_NINE_CHAR_SERVICE_NAME, FOURTEEN_CHAR_HOST_NAME))
+                    .matchesPattern(THIRTY_NINE_CHAR_SERVICE_NAME + "_unit_test_")
+                    .withExpectedSize(63);
+        }
+
+        @Test
+        void shouldNotIncludeServiceHost_andShouldUseShortId_givenFortyCharacterServiceName() {
+            assertDatabaseName(unitTestDatabaseName(FORTY_CHAR_SERVICE_NAME, FOURTEEN_CHAR_HOST_NAME))
+                    .matchesPattern(FORTY_CHAR_SERVICE_NAME + "_ut_")
+                    .withExpectedSize(57);
+        }
+
+        @Test
+        void shouldNotIncludeServiceHost_andShouldUseShortId_givenFortySixCharacterServiceName() {
+            assertDatabaseName(unitTestDatabaseName(FORTY_SIX_CHAR_SERVICE_NAME, FOURTEEN_CHAR_HOST_NAME))
+                    .matchesPattern(FORTY_SIX_CHAR_SERVICE_NAME + "_ut_")
+                    .withExpectedSize(63);
+        }
+
+        @Test
+        void shouldNotIncludeServiceHost_andShouldTrimServiceName_andShouldUseShortId_givenFortySevenCharacterServiceName() {
+            assertDatabaseName(unitTestDatabaseName(FORTY_SEVEN_CHAR_SERVICE_NAME, FOURTEEN_CHAR_HOST_NAME))
+                    .matchesPattern(FORTY_SEVEN_CHAR_SERVICE_NAME.substring(0, 46) + "_ut_")
+                    .withExpectedSize(63);
+        }
+
+        @Test
+        void shouldNotIncludeServiceHost_andShouldTrimServiceName_andShouldUseShortId_givenSixtyCharacterServiceName() {
+            assertDatabaseName(unitTestDatabaseName(SIXTY_CHAR_SERVICE_NAME, FOURTEEN_CHAR_HOST_NAME))
+                    .matchesPattern(SIXTY_CHAR_SERVICE_NAME.substring(0, 46) + "_ut_")
+                    .withExpectedSize(63);
+        }
+
+        @ParameterizedTest
+        @ValueSource(chars = {'/', '\\', '.', ' ', '"', '$', '*', '<', '>', ':', '|', '?'})
+        void shouldReplaceInvalidCharacters(char badChar) {
+            var serviceName = "test" + badChar + "service";
+            assertDatabaseName(unitTestDatabaseName(serviceName, "host1"))
+                    .matchesPattern("test_service_unit_test_host1_");
+        }
+
+        @Test
+        void shouldReplaceMultipleInvalidCharacters() {
+            var serviceName = "a/custom\\test.svc with\"bad$c*h<a>r:a|ct?ers";
+            assertDatabaseName(unitTestDatabaseName(serviceName, "host42"))
+                    .matchesPattern("a_custom_test_svc_with_bad_c_h_a_r_a_ct_ers_ut_");
+        }
+    }
+
+    static final class DatabaseName {
+
+        private final String databaseName;
+
+        private DatabaseName(String databaseName) {
+            this.databaseName = databaseName;
+        }
+
+        static DatabaseName assertDatabaseName(String databaseName) {
+            return new DatabaseName(databaseName);
+        }
+
+        DatabaseName matchesPattern(String expectedPattern) {
+            assertThat(databaseName)
+                    .startsWith(expectedPattern)
+                    .matches(Pattern.compile(expectedPattern + "[0-9]{13,}$"));
+            return this;
+        }
+
+        void withExpectedSize(int expectedSize) {
+            assertThat(databaseName).hasSize(expectedSize);
+        }
+    }
+
+    @Nested
+    class Construction {
+
+        private String hostName;
+        private int port;
+        private String serviceName;
+        private String serviceHost;
+        private String serviceHostMinusDomain;
+
+        @BeforeEach
+        void setUp() {
+            hostName = "mongo-1.acme.com";
+            port = 27_017;
+            serviceName = "test-service";
+            serviceHost = "service-host-1.acme.com";
+            serviceHostMinusDomain = "service-host-1";
+        }
+
+        @ParameterizedTest
+        @EnumSource(HostDomainBehavior.class)
+        void shouldUseConstructor(HostDomainBehavior hostDomainBehavior, SoftAssertions softly) {
+            var properties = new MongoTestProperties(hostName, hostDomainBehavior, port, serviceName, serviceHost);
+
+            assertProperties(softly, properties, hostDomainBehavior);
+        }
+
+        @ParameterizedTest
+        @EnumSource(HostDomainBehavior.class)
+        void shouldUseBuilder(HostDomainBehavior hostDomainBehavior, SoftAssertions softly) {
+            var properties = MongoTestProperties.builder()
+                    .hostName(hostName)
+                    .hostDomainBehavior(hostDomainBehavior)
+                    .port(port)
+                    .serviceName(serviceName)
+                    .serviceHost(serviceHost)
+                    .build();
+
+            assertProperties(softly, properties, hostDomainBehavior);
+        }
+
+        @Test
+        void shouldDefaultToStrippingHostDomain_WhenUsingConstructor(SoftAssertions softly) {
+            var properties = new MongoTestProperties(hostName, null, port, serviceName, serviceHost);
+
+            assertProperties(softly, properties, HostDomainBehavior.STRIP);
+        }
+
+        @Test
+        void shouldDefaultToStrippingHostDomain_WhenUsingBuilder(SoftAssertions softly) {
+            // hostDomainBehavior is not specified; it should use the default
+            var properties = MongoTestProperties.builder()
+                    .hostName(hostName)
+                    .port(port)
+                    .serviceName(serviceName)
+                    .serviceHost(serviceHost)
+                    .build();
+
+            assertProperties(softly, properties, HostDomainBehavior.STRIP);
+        }
+
+        private void assertProperties(SoftAssertions softly,
+                                      MongoTestProperties properties,
+                                      HostDomainBehavior hostDomainBehavior) {
+
+            softly.assertThat(properties.getHostName()).isEqualTo(hostName);
+            softly.assertThat(properties.getHostDomainBehavior()).isEqualTo(hostDomainBehavior);
+            softly.assertThat(properties.getPort()).isEqualTo(port);
+            softly.assertThat(properties.getServiceName()).isEqualTo(serviceName);
+
+            var keepDomain = hostDomainBehavior == HostDomainBehavior.KEEP;
+            var expectedServiceHost = keepDomain ? serviceHost : serviceHostMinusDomain;
+            var expectedDbNamePrefix = keepDomain ? "test-service_unit_test_service-host-1_acme_com_" : "test-service_unit_test_service-host-1_";
+
+            softly.assertThat(properties.getServiceHost()).isEqualTo(expectedServiceHost);
+            softly.assertThat(properties.getDatabaseName())
+                    .startsWith(expectedDbNamePrefix)
+                    .matches(ENDS_WITH_TIMESTAMP_PATTERN);
+            softly.assertThat(properties.getUri())
+                    .startsWith("mongodb://mongo-1.acme.com:27017/" + expectedDbNamePrefix)
+                    .matches(ENDS_WITH_TIMESTAMP_PATTERN);
+        }
+    }
+
+    @Test
+    void shouldSupportMultipleHostNames() {
+        var properties = MongoTestProperties.builder()
+                .hostName(" mongo1.test, mongo2.test ")
+                .port(27_017)
+                .serviceName("test-service")
+                .serviceHost("svc-host-1")
+                .build();
+
+        assertThat(properties.getUri())
+                .startsWith("mongodb://mongo1.test:27017,mongo2.test:27017/test-service_unit_test_svc-host-1_")
+                .matches(Pattern.compile(".*_[0-9]{13,}\\?replicaSet=rs0$"));
+    }
+
+    @Nested
+    class VerifyDatabaseName {
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+                "1234567890",
+                "12345678901234567890",
+                "1234567890123456789012345678901234567890",
+                "123456789012345678901234567890123456789012345678901234567890",
+                "123456789012345678901234567890123456789012345678901234567890123"  // 63 characters
+        })
+        void shouldNotThrow_givenValidDatabaseName(String databaseName) {
+            assertThatCode(() -> MongoTestProperties.verifyDatabaseNameLength(databaseName))
+                    .doesNotThrowAnyException();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+                "1234567890123456789012345678901234567890123456789012345678901234",                 // 64 characters
+                "1234567890123456789012345678901234567890123456789012345678901234567890",           // 70 characters
+                "12345678901234567890123456789012345678901234567890123456789012345678901234567890"  // 80 characters
+        })
+        void shouldThrow_VerifyException_givenInvalidDatabaseName(String databaseName) {
+            assertThatThrownBy(() -> MongoTestProperties.verifyDatabaseNameLength(databaseName))
+                    .isExactlyInstanceOf(VerifyException.class)
+                    .hasMessage("Unexpected error: DB name must be less than 64 characters in length, but was %d: %s",
+                            databaseName.length(), databaseName);
+        }
+    }
+}
