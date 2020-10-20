81ae3d365a51268a27d7b5785479cdaf43060316
Scott Leberknight <scottleber@gmail.com>
2020-10-20 11:47:51 -0400

    Add PostgresLiquibaseTestExtension
    * Add PostgresLiquibaseTestExtension and test
    * Add test-migrations.xml for PostgresLiquibaseTestExtensionTest
    * Add jdbi3-postgres as test-scoped dependency for the test
    
    Fixes #135

---
+++ src/main/java/org/kiwiproject/test/junit/jupiter/PostgresLiquibaseTestExtension.java
@@ -0,0 +1,118 @@
+package org.kiwiproject.test.junit.jupiter;
+
+import static java.util.Objects.nonNull;
+import static org.kiwiproject.base.KiwiStrings.f;
+
+import io.zonky.test.db.postgres.embedded.LiquibasePreparer;
+import io.zonky.test.db.postgres.junit5.EmbeddedPostgresExtension;
+import io.zonky.test.db.postgres.junit5.PreparedDbExtension;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.kiwiproject.test.jdbc.JdbcTests;
+import org.kiwiproject.test.jdbc.SimpleSingleConnectionDataSource;
+
+/**
+ * This JUnit Jupiter extension uses the Embedded Postgres Jupiter extension but always returns a
+ * {@link SimpleSingleConnectionDataSource}. This ensures that classes under test all use the same
+ * JDBC {@code Connection} which is useful when the test code executes in a transaction and thus
+ * all those classes can all see the uncommitted data.
+ * <p>
+ * When registering this extension, you must use a {@code static} field because both {@link BeforeAllCallback}
+ * and {@link AfterAllCallback} must be registered at the class level.
+ * <p>
+ * Example usage showing this extension used to supply the {@code DataSource} for a {@link Jdbi3Extension}:
+ * <pre>
+ * class MyDatabaseTest {
+ *
+ *    {@literal @}RegisterExtension
+ *     static final PostgresLiquibaseTestExtension DATABASE_EXTENSION =
+ *             new PostgresLiquibaseTestExtension("migrations.xml");
+ *
+ *    {@literal @}RegisterExtension
+ *     final Jdbi3Extension jdbi3Extension =  Jdbi3Extension.builder()
+ *             .dataSource(DATABASE_EXTENSION.getDataSource())
+ *             .plugin(new PostgresPlugin())
+ *             .build();
+ *
+ *     // ...tests...
+ * }
+ * </pre>
+ * Note in the above that the {@link PostgresLiquibaseTestExtension} is declared both static and final, while the
+ * {@link Jdbi3Extension} is not static but is final. This configuration ensures that the embedded Postgres database
+ * is setup only one time before all tests run. The {@link Jdbi3Extension} is initialized before each test with
+ * a new transaction that is rolled back after each test, which again ensures code running in the transaction
+ * participates in the same transaction and can see uncommitted data, but also ensures no data is actually committed
+ * since the transaction is rolled back. Each test therefore does not need to worry about cleaning up any data from
+ * previous tests.
+ *
+ * @see PreparedDbExtension
+ * @see SimpleSingleConnectionDataSource
+ */
+@Slf4j
+public class PostgresLiquibaseTestExtension implements BeforeAllCallback, AfterAllCallback {
+
+    private static final String EMPTY_PASSWORD = "";
+
+    /**
+     * The embedded Postgres extension. Generally you should not need this directly when using this extension, but
+     * we are exposing it just in case and because this is a testing library, not a production API.
+     */
+    @Getter
+    private final PreparedDbExtension postgres;
+
+    /**
+     * The test DataSource, which is a {@link SimpleSingleConnectionDataSource}.
+     */
+    @Getter
+    private SimpleSingleConnectionDataSource testDataSource;
+
+    /**
+     * Construct a new instance using the given classpath location of a Liquibase migrations file.
+     *
+     * @param migrationClassPathLocation classpath location of Liquibase migrations file
+     */
+    public PostgresLiquibaseTestExtension(String migrationClassPathLocation) {
+        LOG.trace("Constructing new instance for migration path: {}", migrationClassPathLocation);
+
+        var liquibasePreparer = LiquibasePreparer.forClasspathLocation(migrationClassPathLocation);
+        postgres = EmbeddedPostgresExtension.preparedDatabase(liquibasePreparer);
+    }
+
+    /**
+     * Start the embedded Postgres extension and initialize a test DataSource that connects to it. This test
+     * DataSource should be used in your tests to obtain a Connection.
+     *
+     * @param context the extension context
+     * @throws Exception if any error occurs initializing the embedded Postgres or connecting to it
+     */
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+        LOG.trace("Invoke PreparedDbExtension.beforeAll() to initialize the embedded Postgres");
+        postgres.beforeAll(context);
+
+        var connectionInfo = postgres.getConnectionInfo();
+        var url = f("jdbc:postgresql://localhost:{}/{}", connectionInfo.getPort(), connectionInfo.getDbName());
+        var user = connectionInfo.getUser();
+        LOG.trace("Initializing new single-connection test DataSource for URL {} and user {}", url, user);
+        testDataSource = JdbcTests.newTestDataSource(url, user, EMPTY_PASSWORD);
+    }
+
+    /**
+     * Closes the Connection provided by the test DataSource, then shuts down the embedded Postgres.
+     *
+     * @param context the extension context
+     */
+    @Override
+    public void afterAll(ExtensionContext context) {
+        if (nonNull(testDataSource)) {
+            LOG.trace("Closing test DataSource");
+            testDataSource.close();
+        }
+
+        LOG.trace("Invoke PreparedDbExtension.afterAll() to shut down the embedded Postgres");
+        postgres.afterAll(context);
+    }
+}
---
+++ src/test/java/org/kiwiproject/test/junit/jupiter/PostgresLiquibaseTestExtensionTest.java
@@ -0,0 +1,125 @@
+package org.kiwiproject.test.junit.jupiter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+import lombok.Value;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+import org.jdbi.v3.postgres.PostgresPlugin;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.kiwiproject.test.jdbi.Jdbi3GeneratedKeys;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@DisplayName("PostgresLiquibaseTestExtension")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SuppressWarnings({"SqlNoDataSourceInspection", "SqlDialectInspection"})
+class PostgresLiquibaseTestExtensionTest {
+
+    @RegisterExtension
+    static final PostgresLiquibaseTestExtension POSTGRES =
+            new PostgresLiquibaseTestExtension("PostgresLiquibaseTestExtensionTest/test-migrations.xml");
+
+    @RegisterExtension
+    final Jdbi3Extension jdbi3Extension = Jdbi3Extension.builder()
+            .dataSource(POSTGRES.getTestDataSource())
+            .plugin(new PostgresPlugin())
+            .build();
+
+    private Jdbi jdbi;
+    private Handle handle;
+
+    @BeforeEach
+    void setUp() {
+        jdbi = jdbi3Extension.getJdbi();
+        handle = jdbi3Extension.getHandle();
+    }
+
+    @Test
+    @Order(1)
+    void shouldSetProperties() {
+        assertThat(POSTGRES.getTestDataSource()).isNotNull();
+        assertThat(POSTGRES.getPostgres()).isNotNull();
+    }
+
+    @Test
+    @Order(2)
+    void shouldAlwaysReturnSameConnection() {
+        var handle1 = jdbi.open();
+        var handle2 = jdbi.open();
+
+        assertThat(handle1.getConnection())
+                .isSameAs(handle2.getConnection())
+                .isSameAs(handle.getConnection());
+    }
+
+    @Test
+    @Order(3)
+    void shouldUseSameConnection() {
+        handle.createUpdate("create table test_table (name text, age int)").execute();
+
+        jdbi.withHandle(h -> h.createUpdate("insert into test_table (name, age) values ('Bob', 42)")).execute();
+        jdbi.withHandle(h -> h.createUpdate("insert into test_table (name, age) values ('Alice', 38)")).execute();
+        jdbi.useHandle(h -> {
+            var names = h.createQuery("select name from test_table order by name asc").mapTo(String.class).list();
+            assertThat(names).containsExactly("Alice", "Bob");
+        });
+    }
+
+    @Test
+    @Order(4)
+    void shouldInsertData() {
+        var update1 = handle.createUpdate("insert into sample_table (col_1, col_2) values ('A1', 'A2')");
+        var id1 = Jdbi3GeneratedKeys.executeAndGenerateId(update1);
+
+        var update2 = handle.createUpdate("insert into sample_table (col_1, col_2) values ('B1', 'B2')");
+        var id2 = Jdbi3GeneratedKeys.executeAndGenerateId(update2);
+
+        var samples = handle.createQuery("select * from sample_table order by col_1")
+                .map(new SampleMapper())
+                .list();
+
+        assertThat(samples).extracting("id", "col1", "col2").containsExactly(
+                tuple(id1, "A1", "A2"),
+                tuple(id2, "B1", "B2")
+        );
+    }
+
+    @Test
+    @Order(5)
+    void shouldNotSeeDataFromPreviousTest() {
+        var count = handle.createQuery("select count(*) as sample_count from sample_table")
+                .mapTo(Integer.class)
+                .first();
+
+        assertThat(count).isZero();
+    }
+
+    @Value
+    private static class Sample {
+        Long id;
+        String col1;
+        String col2;
+    }
+
+    private static class SampleMapper implements RowMapper<Sample> {
+        @Override
+        public Sample map(ResultSet rs, StatementContext ctx) throws SQLException {
+            return new Sample(
+                    rs.getLong("id"),
+                    rs.getString("col_1"),
+                    rs.getString("col_2")
+            );
+        }
+    }
+}
