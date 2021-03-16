c728117c61590d4cba086caed809d90c3032de46
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2021-03-16 17:35:14 -0400

    Add KiwiAssertJ methods to assert one element in collections and maps
    * <T> ObjectAssert<T> assertThatOnlyElementIn(Iterable<T> iterable)
    * <T> T assertAndGetOnlyElementIn(Iterable<T> iterable)
    * <K, V> Map.Entry<K, V> assertAndGetOnlyEntryIn(Map<K, V> map)
    
    The first method returns ObjectAssert for further method chaining.
    The second and third methods simply assert the collection/map contains
    exactly one element/entry and then returns that object.
    
    Closes #205

--- src/main/java/org/kiwiproject/test/assertj/KiwiAssertJ.java
+++ src/main/java/org/kiwiproject/test/assertj/KiwiAssertJ.java
@@ -3,6 +3,9 @@ package org.kiwiproject.test.assertj;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import lombok.experimental.UtilityClass;
+import org.assertj.core.api.ObjectAssert;
+
+import java.util.Map;
 
 /**
  * Some AssertJ test utilities that you might or might find useful.
@@ -38,5 +41,52 @@ public class KiwiAssertJ {
     public static <T> T assertIsTypeOrSubtype(Object object, Class<T> type) {
         assertThat(object).isInstanceOf(type);
         return type.cast(object);
+    }
+
+    /**
+     * Assert the given Iterable contains exactly one element, and returns {@link ObjectAssert} for further
+     * AssertJ method chaining.
+     * <p>
+     * This is basically just a shortcut for {@code assertThat(iterable).hasSize(1).first()} but that seems to come up
+     * enough in testing (for me anyway) that I don't want to type it out every time.
+     *
+     * @param iterable the Iterable
+     * @param <T>      the type of element
+     * @return an {@link ObjectAssert} for further method chaining
+     */
+    public static <T> ObjectAssert<T> assertThatOnlyElementIn(Iterable<T> iterable) {
+        return assertThat(iterable)
+                .describedAs("Expected exactly one element")
+                .hasSize(1)
+                .first();
+    }
+
+    /**
+     * Assert the given Iterable contains exactly one element, and return that element for further inspection.
+     *
+     * @param iterable the Iterable
+     * @param <T>      the type of element
+     * @return the first element
+     */
+    public static <T> T assertAndGetOnlyElementIn(Iterable<T> iterable) {
+        assertThat(iterable)
+                .describedAs("Expected exactly one element")
+                .hasSize(1);
+        return iterable.iterator().next();
+    }
+
+    /**
+     * Assert the given Map contains exactly one entry, and return that entry for further inspection.
+     *
+     * @param map the Map
+     * @param <K> the type of keys
+     * @param <V> the type of values
+     * @return the first entry
+     */
+    public static <K, V> Map.Entry<K, V> assertAndGetOnlyEntryIn(Map<K, V> map) {
+        assertThat(map)
+                .describedAs("Expected exactly one entry")
+                .hasSize(1);
+        return map.entrySet().iterator().next();
     }
 }
