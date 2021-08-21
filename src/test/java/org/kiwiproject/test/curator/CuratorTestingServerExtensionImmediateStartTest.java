6c1373ad5e76a987271fd7d2ea5a680ff2067ffc
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2021-08-20 23:50:21 -0400

    Add ability to immediately start ZK testing server in Curator extension (#242)
    * Add two factory methods to CuratorTestingServerExtension that will
      start the Curator testing server immediately (at construction) instead
      of during the JUnit lifecycle (i.e. in beforeAll). These are named
      newStartingImmediately and one will start the testing server on a
      random port and the other takes a specific port to use.
    * Before attempting to start the testing server, check that the port
      is available. Otherwise, when Curator's TestingServer attempts to
      start, it will never complete. The reason is that TestingServer starts
      the server in a new Thread and then blocks (without a timeout) until
      connected. We don't want this behavior in a test extension and instead
      should fail fast.
    * Restructure existing test to verify nested test class behavior
      separate from tests having only top-level tests
    * Add new test specifically to test the immediate start (necessary since
      we have to register the CuratorTestingServerExtension with immediate
      start explicitly)
    
    Test restructuring:
    
    Split the original CuratorTestingServerExtensionTest test into two
    separate tests.
    
    The first, CuratorTestingServerExtensionTest, contains only top-level
    tests while the second, CuratorTestingServerExtensionNestedTest,
    contains both top-level and nested test classes. These two contain
    essentially the exact same tests, to ensure we are agnostic of how a
    test is structured. Though as noted in extension's Javadoc, nested test
    classes will cause us to use multiple testing servers (due to the way
    the JUnit lifecycle works when using nested test classes).
    
    Add CuratorTestingServerExtensionImmediateStartTest to explicitly test
    the immediate start feature added in this commit.
    
    Closes #239

---
+++ src/test/java/org/kiwiproject/test/curator/CuratorTestingServerExtensionImmediateStartTest.java
@@ -0,0 +1,128 @@
+package org.kiwiproject.test.curator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.imps.CuratorFrameworkState;
+import org.apache.curator.retry.RetryOneTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This tests the case when a test has requested the Curator {@link org.apache.curator.test.TestingServer} to start
+ * immediately, instead of waiting for the @{@link org.junit.jupiter.api.extension.BeforeAllCallback} during
+ * the normal JUnit Jupiter lifecycle. To do this, it uses a custom Jupiter extension which attempts to connect to
+ * the testing ZooKeeper server. This test also contains a nested test class to ensure everything works regardless
+ * of the test structure.
+ */
+@DisplayName("CuratorTestingServerExtension with immediate start")
+@Slf4j
+class CuratorTestingServerExtensionImmediateStartTest {
+
+    @RegisterExtension
+    static final CuratorTestingServerExtension ZK_TEST_SERVER = CuratorTestingServerExtension.newStartingImmediately();
+
+    /**
+     * Simulates another extension that requires a ZooKeeper server. Since {@link org.apache.curator.test.TestingServer}
+     * doesn't expose any way to check its state (i.e. is it started or not), this gets the {@link CuratorFramework}
+     * client from the {@link CuratorTestingServerExtension} and attempts to start it, blocking until connected (with
+     * a timeout to ensure it stops if it cannot connect). This is an indirect way of determining whether the testing
+     * server has started.
+     */
+    @Slf4j
+    static class ZooKeeperUsingExtension implements BeforeAllCallback {
+
+        @Getter
+        private boolean testingServerStarted;
+
+        /**
+         * Creates a new Curator client and attempts to connect to the testing server.
+         */
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            var sessionTimeoutMs = 2_000;
+            var connectionTimeoutMs = 1_000;
+            var sleepMsBetweenRetry = 500;
+            var retryPolicy = new RetryOneTime(sleepMsBetweenRetry);
+
+            try (var client = CuratorFrameworkFactory.newClient(ZK_TEST_SERVER.getConnectString(),
+                    sessionTimeoutMs,
+                    connectionTimeoutMs,
+                    retryPolicy)) {
+                client.start();
+                testingServerStarted = client.blockUntilConnected(1, TimeUnit.SECONDS);
+            }
+        }
+    }
+
+    @RegisterExtension
+    static final ZooKeeperUsingExtension ZOOKEEPER_USING_EXTENSION = new ZooKeeperUsingExtension();
+
+    private CuratorFramework client;
+
+    @BeforeEach
+    void setUp(TestInfo testInfo) {
+        LOG.trace("Test: {}", testInfo.getDisplayName());
+
+        client = ZK_TEST_SERVER.getClient();
+    }
+
+    @Test
+    void shouldHaveStartedTestingServerImmediately() {
+        assertThat(ZOOKEEPER_USING_EXTENSION.isTestingServerStarted())
+                .describedAs("The TestingServer should have started immediately (when constructed) but did not")
+                .isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-42, -1, 65_536, 65_537, 90_000})
+    void shouldRejectInvalidPorts(int value) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new CuratorTestingServerExtension(value))
+                .withMessage("Invalid port: " + value);
+    }
+
+    @Test
+    void shouldHavePort() {
+        assertThat(ZK_TEST_SERVER.getPort()).isPositive();
+    }
+
+    @Test
+    void shouldHaveClient() {
+        assertThat(client.getState()).isEqualTo(CuratorFrameworkState.STARTED);
+    }
+
+    @Nested
+    class CanUseClient {
+
+        @Test
+        void toCheckExistenceOfPath() throws Exception {
+            var stat = client.checkExists().forPath("/foo");
+            assertThat(stat).isNull();
+        }
+
+        @Test
+        void toCreateZnode() throws Exception {
+            var path = "/bar/baz";
+            var createdPath = client.create().creatingParentContainersIfNeeded().forPath(path);
+            assertThat(createdPath).isEqualTo(path);
+
+            var stat = client.checkExists().forPath(createdPath);
+            assertThat(stat).isNotNull();
+        }
+    }
+}
