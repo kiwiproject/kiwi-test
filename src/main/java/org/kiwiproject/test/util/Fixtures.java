814e856cdbb30d37a40dbf86fa03355ca56727e9
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2022-05-03 08:23:42 -0400

    Switch to build on Java 17 and Temurin (#303)
    * Switch to build on Java 17 and Temurin
    
    * Add lovely hack to allow this to work the same on JDK 11-16 and 17+
    
    The hack in Fixtures#fixture(String, Charset) adds a new catch block
    that catches Error. When the Error's cause is a CharacterCodingException
    we throw an UncheckedIOException with the CharacterCodingException as
    its cause. This replicates the behavior from JDK 11-16 when running on a
    17 JDK and presumably 18, 19, etc. I am pretty sure this is a regression
    in the JDK's Files#readString method because its Javadoc says that an
    IOException is thrown for malformed input or unmappable byte sequence.
    
    But, since I certainly can't control the JDK and don't even know how to
    go about reporting or asking whether this is a real problem, for now
    I've just hacked around it.
    
    Since our code coverage is only run on JDK 11 at present, I also
    extracted a separate method that encapsulates this special handling code
    in order to fully test it on JDKs before 17.
    
    * Just call getCause() on the Error object
    
    This avoids generating the stack trace as a String, which is what
    ThrowableInfo#of will do. Since we only need the cause of the Throwable,
    we can just get the cause directly from the Error object. Not sure why
    I initially used ThrowableInfo.

--- src/main/java/org/kiwiproject/test/util/Fixtures.java
+++ src/main/java/org/kiwiproject/test/util/Fixtures.java
@@ -1,7 +1,10 @@
 package org.kiwiproject.test.util;
 
+import static java.util.Objects.nonNull;
+
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.io.Resources;
+import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import org.kiwiproject.net.UncheckedURISyntaxException;
 
@@ -10,6 +13,7 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -19,7 +23,6 @@ import java.nio.file.Paths;
 /**
  * Helper methods for working with test fixtures.
  */
-@SuppressWarnings("UnstableApiUsage")
 @UtilityClass
 public class Fixtures {
 
@@ -37,15 +40,29 @@ public class Fixtures {
     }
 
     /**
-     * Reads the given fixture file from the classpath (e. g. {@code src/test/resources})
+     * Reads the given fixture file from the classpath (e.g. {@code src/test/resources})
      * and returns its contents as a string.
      *
      * @param resourceName the name/path of to the classpath resource
      * @param charset      the charset of the fixture file
      * @return the fixture contents
      * @throws UncheckedURISyntaxException if the resource name/path is invalid as a URI
-     * @throws UncheckedIOException        if an I/O error occurs
+     * @throws UncheckedIOException        if an I/O error occurs; the cause will be the actual {@link IOException}
+     * @implNote As of Java 17, {@link Files#readString(Path, Charset)} throws an {@link Error} instead of
+     * an {@link IOException} in the case of malformed input or unmappable characters. This {@link Error} contains a cause
+     * of {@link IOException}, with actual type of either {@link java.nio.charset.MalformedInputException MalformedInputException}
+     * or {@link java.nio.charset.UnmappableCharacterException UnmappableCharacterException}, both of which are subclasses of
+     * {@link CharacterCodingException}. Before Java 17 (we've tested on 11 and 16), this method threw an
+     * {@link IOException} whose actual type was one of the previously mentioned {@link CharacterCodingException}
+     * subclasses. This could be a bug in the JDK, or it could be a misunderstanding of how
+     * {@link Files#readString(Path, Charset)} is supposed to work. However, its Javadoc clearly states that it throws
+     * an {@link IOException} "if an I/O error occurs reading from the file or a malformed or unmappable byte
+     * sequence is read". In our tests of malformed input and unmappable characters, we see the {@link Error} thrown
+     * instead of a {@link CharacterCodingException} on JDKs prior to 17. As a result, we have included code to handle
+     * the specific case when an {@link Error} is thrown and its cause is a {@link CharacterCodingException}.
      */
+    @SneakyThrows
+    @SuppressWarnings("java:S1181")  // Suppress Sonar "Throwable and Error should not be caught" warning (see implNote)
     public static String fixture(String resourceName, Charset charset) {
         try {
             var url = Resources.getResource(resourceName);
@@ -53,7 +70,27 @@ public class Fixtures {
             return Files.readString(path, charset);
         } catch (IOException e) {
             throw new UncheckedIOException("Error reading fixture: " + resourceName, e);
+        } catch (Error error) {
+            // Special handling for JDK 17+ (see implementation note above). Also, this relies on the "SneakyThrows".
+            throw uncheckedIOExceptionOrOriginalError(error, resourceName);
         }
+    }
+
+    /**
+     * This method only exists to permit testing it when building on JDK 11-16, since those throw UncheckedIOException
+     * whereas JDK 17 throws an Error (see above implNote in {@link #fixture(String, Charset)}).
+     *
+     * @param error        the original Error
+     * @param resourceName the resource name, only used when UncheckedIOException is returned
+     * @return the original {@code error} or a new UncheckedIOException
+     */
+    @VisibleForTesting
+    static Throwable uncheckedIOExceptionOrOriginalError(Error error, String resourceName) {
+        var cause = error.getCause();
+        if (nonNull(cause) && cause instanceof CharacterCodingException) {
+            return new UncheckedIOException("Error reading fixture: " + resourceName, (CharacterCodingException) cause);
+        }
+        return error;
     }
 
     /**
