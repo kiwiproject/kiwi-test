ab3dea27352a8d303347f974770bba3928faa195
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2022-07-10 17:14:06 -0400

    Overload isNear methods in JavaTimeArgumentMatchers with ZonedDateTime
    Add three new overloads of isNear in JavaTimeArgumentMatchers that
    accept ZonedDateTime instead of Instant.
    
    There is probably a way to make the code contain less duplication, e.g.
    by using the common Temporal supertype of Instant and ZonedDateTime.
    But for now, this first implementation leaves the existing Instant-based
    methods alone.
    
    So, both the production code and the tests are almost identical
    except for the type, ZonedDateTime vs. Instant. Again, not ideal, but
    for now it is the "safer" choice for methods that I don't expect to
    change much, if ever, from now on, since the existing production methods
    are the same.
    
    In the future, we could revisit refactoring this code if, for example,
    we need to add more overloads that accept additional temporal types such
    as LocalDateTime, OffsetDateTime, and so on.
    
    Closes #282

--- src/main/java/org/kiwiproject/test/mockito/JavaTimeArgumentMatchers.java
+++ src/main/java/org/kiwiproject/test/mockito/JavaTimeArgumentMatchers.java
@@ -11,6 +11,8 @@ import org.mockito.ArgumentMatcher;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 
 /**
  * Static utility methods to create {@link ArgumentMatcher} instances for types in the {@code java.time} package.
@@ -20,6 +22,85 @@ import java.time.Instant;
 public class JavaTimeArgumentMatchers {
 
     private static final int DEFAULT_SLACK_MILLIS = 500;
+    private static final String EXPECTED_TIME_CANNOT_BE_NULL = "expectedTime cannot be null";
+    private static final String SLACK_CANNOT_BE_NULL = "slack cannot be null";
+
+    /**
+     * Matches an {@link ZonedDateTime} near the given expected time within +/- 500 milliseconds.
+     * <p>
+     * In other words, the actual time must be in the range {@code [ expectedTime - 500ms , expectedTime + 500ms ]}.
+     * <p>
+     * Example usage:
+     * <pre>
+     * // Assume a Timekeeper class contains an announceTime(ZonedDateTime time) method
+     * var expectedTime = determineExpectedZonedDateTime();
+     * verify(timekeeper).announceTime(argThat(isNear(expectedTime));
+     * </pre>
+     *
+     * @param expectedTime the expected time
+     * @return a new {@link ArgumentMatcher} instance
+     */
+    public static ArgumentMatcher<ZonedDateTime> isNear(ZonedDateTime expectedTime) {
+        return isNear(expectedTime, DEFAULT_SLACK_MILLIS);
+    }
+
+    /**
+     * Matches an {@link ZonedDateTime} near the given expected time within +/- {@code slack} duration.
+     * <p>
+     * In other words, the actual time must be in the range {@code [ expectedTime - slack , expectedTime + slack ]}.
+     * <p>
+     * Example usage:
+     * <pre>
+     * // Assume a Timekeeper class contains an announceTime(ZonedDateTime time) method
+     * var expectedTime = determineExpectedZonedDateTime();
+     * var slack = Duration.ofMillis(250)
+     * verify(timekeeper).announceTime(argThat(isNear(expectedTime, slack));
+     * </pre>
+     *
+     * @param expectedTime the expected time
+     * @param slack        the amount of time the actual time can differ before or after the expected time
+     * @return a new {@link ArgumentMatcher} instance
+     */
+    public static ArgumentMatcher<ZonedDateTime> isNear(ZonedDateTime expectedTime, Duration slack) {
+        checkArgumentNotNull(slack, SLACK_CANNOT_BE_NULL);
+        return isNear(expectedTime, slack.toMillis());
+    }
+
+    /**
+     * Matches an {@link ZonedDateTime} near the given expected time within +/- {@code slack} milliseconds.
+     * <p>
+     * In other words, the actual time must be in the range {@code [ expectedTime - slackMillis , expectedTime + slackMillis ]}.
+     * <pre>
+     * // Assume a Timekeeper class contains an announceTime(ZonedDateTime time) method
+     * var expectedTime = determineExpectedZonedDateTime();
+     * var slackMillis = 300;
+     * verify(timekeeper).announceTime(argThat(isNear(expectedTime, slackMillis));
+     * </pre>
+     *
+     * @param expectedTime the expected time
+     * @param slackMillis  the number of milliseconds the actual time can differ before or after the expected time
+     * @return a new {@link ArgumentMatcher} instance
+     */
+    public static ArgumentMatcher<ZonedDateTime> isNear(ZonedDateTime expectedTime, long slackMillis) {
+        checkArgumentNotNull(expectedTime, EXPECTED_TIME_CANNOT_BE_NULL);
+        checkPositive(slackMillis);
+
+        return actualTime -> {
+            LOG.trace("expectedTime: {} ; actualTime: {} ; Duration.between(expectedTimeTime, actualTime): {}",
+                    expectedTime,
+                    actualTime,
+                    lazy(() -> Duration.between(expectedTime, actualTime)));
+
+            var lowerBound = expectedTime.minus(slackMillis, ChronoUnit.MILLIS);
+            var upperBound = expectedTime.plus(slackMillis, ChronoUnit.MILLIS);
+
+            assertThat(actualTime)
+                    .describedAs("actual time %s not between [ %s, %s ]", actualTime, lowerBound, upperBound)
+                    .isBetween(lowerBound, upperBound);
+
+            return true;
+        };
+    }
 
     /**
      * Matches an {@link Instant} near the given expected time within +/- 500 milliseconds.
@@ -58,7 +139,7 @@ public class JavaTimeArgumentMatchers {
      * @return a new {@link ArgumentMatcher} instance
      */
     public static ArgumentMatcher<Instant> isNear(Instant expectedTime, Duration slack) {
-        checkArgumentNotNull(slack, "slack cannot be null");
+        checkArgumentNotNull(slack, SLACK_CANNOT_BE_NULL);
 
         return isNear(expectedTime, slack.toMillis());
     }
@@ -79,7 +160,7 @@ public class JavaTimeArgumentMatchers {
      * @return a new {@link ArgumentMatcher} instance
      */
     public static ArgumentMatcher<Instant> isNear(Instant expectedTime, long slackMillis) {
-        checkArgumentNotNull(expectedTime, "expectedTime cannot be null");
+        checkArgumentNotNull(expectedTime, EXPECTED_TIME_CANNOT_BE_NULL);
         checkPositive(slackMillis);
 
         return actualTime -> {
