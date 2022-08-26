7c878aa1808a7e26526c193a7a92aaf12c384bb5
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2022-08-26 20:23:41 +0000

    Remove unused import in H2FileBasedDatabaseExtensionUtilitiesTest

--- src/test/java/org/kiwiproject/test/junit/jupiter/H2FileBasedDatabaseExtensionUtilitiesTest.java
+++ src/test/java/org/kiwiproject/test/junit/jupiter/H2FileBasedDatabaseExtensionUtilitiesTest.java
@@ -1,6 +1,5 @@
 package org.kiwiproject.test.junit.jupiter;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
