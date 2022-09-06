79a7b88ddda0b6f2d5c5047d4b6e6f8884ec4ea4
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2022-09-06 09:48:13 -0400

    Add H2LiquibaseExtension (#338)
    Closes #332

---
+++ src/test/java/org/kiwiproject/test/junit/jupiter/H2LiquibaseExtensionTest.java
@@ -0,0 +1,149 @@
+package org.kiwiproject.test.junit.jupiter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.jdbi.v3.core.Jdbi;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.kiwiproject.test.h2.H2FileBasedDatabase;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@SuppressWarnings({"SqlResolve", "SqlNoDataSourceInspection"})
+@DisplayName("H2LiquibaseExtension")
+class H2LiquibaseExtensionTest {
+
+    @RegisterExtension
+    static final H2LiquibaseExtension H2_LIQUIBASE_EXTENSION =
+            new H2LiquibaseExtension("H2LiquibaseExtensionTest/test-migrations.xml");
+
+    private Connection conn;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        conn = H2_LIQUIBASE_EXTENSION.getDataSource().getConnection();
+        conn.setAutoCommit(false);
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        conn.rollback();
+    }
+
+    @Test
+    void shouldProvideH2FileBasedDatabase() {
+        var database = H2_LIQUIBASE_EXTENSION.getDatabase();
+        assertThat(database.getUrl()).startsWith("jdbc:h2:");
+        assertThat(database.getDataSource()).isNotNull();
+        assertThat(database.getDirectory()).isDirectory();
+    }
+
+    @Test
+    void shouldProvideH2FileBaseDatabaseInParameter(@H2Database H2FileBasedDatabase database) {
+        assertThat(database).isSameAs(H2_LIQUIBASE_EXTENSION.getDatabase());
+    }
+
+    @Test
+    void shouldAllowDirectAccessToH2FileBasedDatabaseExtension() {
+        assertThat(H2_LIQUIBASE_EXTENSION.getH2Extension()).isNotNull();
+
+        assertThat(H2_LIQUIBASE_EXTENSION.getH2Extension().getDatabase())
+                .isSameAs(H2_LIQUIBASE_EXTENSION.getDatabase());
+    }
+
+    @Test
+    void shouldRunMigrationsBeforeAllTests() throws SQLException {
+        try (var stmt = conn.createStatement();
+             var rs = stmt.executeQuery("select * from databasechangelog order by orderexecuted")) {
+
+            var ids = new ArrayList<String>();
+            while (rs.next()) {
+                ids.add(rs.getString("id"));
+            }
+
+            assertThat(ids).containsExactly("0001_create_sample_table", "0002_create_another_table");
+        }
+    }
+
+    @Test
+    void shouldUseConnectionToInsertData() throws SQLException {
+        try (var stmt = conn.createStatement()) {
+            var count = stmt.executeUpdate("insert into sample_table (col_1, col_2) values ('a', 'b')");
+            assertThat(count).isOne();
+        }
+    }
+
+    @Test
+    void shouldUseConnectionToQueryData() throws SQLException {
+        try (var ps = conn.prepareStatement("insert into sample_table (col_1, col_2) values (?, ?)")) {
+            ps.setString(1, "value 1");
+            ps.setString(2, "value 2");
+            var updateCount = ps.executeUpdate();
+            assertThat(updateCount).isOne();
+        }
+
+        try (var stmt = conn.createStatement(); var rs = stmt.executeQuery("select * from sample_table")) {
+            assertThat(rs.next()).isTrue();
+            assertThat(rs.getString("col_1")).isEqualTo("value 1");
+            assertThat(rs.getString("col_2")).isEqualTo("value 2");
+        }
+    }
+
+    @Nested
+    class ParameterResolutionInNestedTests {
+
+        @Test
+        void shouldProvideH2FileBaseDatabaseInParameter(@H2Database H2FileBasedDatabase database) {
+            assertThat(database).isSameAs(H2_LIQUIBASE_EXTENSION.getDatabase());
+        }
+    }
+
+    @Nested
+    class UsingJdbi {
+
+        private Jdbi jdbi;
+
+        @BeforeEach
+        void setUp() {
+            jdbi = Jdbi.create(conn);
+        }
+
+        @Test
+        void shouldInsertData() {
+            jdbi.useHandle(handle -> {
+                var updateCount1 = handle.createUpdate("insert into sample_table (col_1, col_2) values (?, ?)")
+                        .bind(0, "value 1")
+                        .bind(1, "value 2")
+                        .execute();
+                assertThat(updateCount1).isOne();
+
+                var updateCount2 = handle.createUpdate("insert into another_table (another_col_1, another_col_2) values (?, ?)")
+                        .bind(0, "another value 1")
+                        .bind(1, 42)
+                        .execute();
+                assertThat(updateCount2).isOne();
+            });
+        }
+
+        @Test
+        void shouldQueryData() {
+            jdbi.useHandle(handle -> {
+                var sql = "insert into sample_table (col_1, col_2) values (?, ?)";
+                handle.createUpdate(sql).bind(0, "val 1").bind(1, "val 2").execute();
+                handle.createUpdate(sql).bind(0, "val 1").bind(1, "val 2").execute();
+                handle.createUpdate(sql).bind(0, "val 1").bind(1, "val 2").execute();
+            });
+
+            var count = jdbi.withHandle(handle ->
+                    handle.createQuery("select count(*) from sample_table").mapTo(Long.TYPE).one());
+
+            assertThat(count).isEqualTo(3);
+        }
+    }
+}
---
+++ src/test/java/org/kiwiproject/test/junit/jupiter/H2LiquibaseExtensionWithJdbiExtensionTest.java
@@ -0,0 +1,145 @@
+package org.kiwiproject.test.junit.jupiter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+import lombok.Value;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.kiwiproject.test.jdbi.Jdbi3GeneratedKeys;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@DisplayName("H2LiquibaseExtension with JdbiExtension")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SuppressWarnings({"SqlNoDataSourceInspection", "SqlResolve"})
+class H2LiquibaseExtensionWithJdbiExtensionTest {
+
+    @RegisterExtension
+    static final H2LiquibaseExtension H2_LIQUIBASE_EXTENSION =
+            new H2LiquibaseExtension("H2LiquibaseExtensionTest/test-migrations.xml");
+
+    @RegisterExtension final Jdbi3Extension jdbi3Extension = Jdbi3Extension.builder()
+            .dataSource(H2_LIQUIBASE_EXTENSION.getDataSource())
+            .build();
+
+    private Handle handle;
+
+    @BeforeEach
+    void setUp() {
+        handle = jdbi3Extension.getHandle();
+    }
+
+    @Test
+    @Order(1)
+    void shouldInsertData() {
+        var update1 = handle.createUpdate("insert into sample_table (col_1, col_2) values ('A1', 'A2')");
+        var id1 = Jdbi3GeneratedKeys.executeAndGenerateId(update1);
+
+        var update2 = handle.createUpdate("insert into sample_table (col_1, col_2) values ('B1', 'B2')");
+        var id2 = Jdbi3GeneratedKeys.executeAndGenerateId(update2);
+
+        var samples = handle.createQuery("select * from sample_table order by col_1")
+                .map(new SampleMapper())
+                .list();
+
+        assertThat(samples).extracting("id", "col1", "col2").containsExactly(
+                tuple(id1, "A1", "A2"),
+                tuple(id2, "B1", "B2")
+        );
+    }
+
+    @Test
+    @Order(2)
+    void shouldNotSeeDataFromPreviousTest() {
+        var count = handle.createQuery("select count(*) as sample_count from sample_table")
+                .mapTo(Integer.class)
+                .first();
+
+        assertThat(count).isZero();
+    }
+
+    @Nested
+    class NestedTests {
+
+        @Test
+        @Order(3)
+        void shouldQueryAnotherTable() {
+            var samples = handle.createQuery("select * from another_table")
+                    .map(new AnotherSampleMapper())
+                    .list();
+
+            assertThat(samples).isEmpty();
+        }
+
+        @Test
+        @Order(4)
+        void shouldInsertAndQueryAnotherTable() {
+            var update1 = handle.createUpdate("insert into another_table (another_col_1, another_col_2) values ('C1', 42)");
+            var id1 = Jdbi3GeneratedKeys.executeAndGenerateId(update1);
+
+            var update2 = handle.createUpdate("insert into another_table (another_col_1, another_col_2) values ('D1', 84)");
+            var id2 = Jdbi3GeneratedKeys.executeAndGenerateId(update2);
+
+            var update3 = handle.createUpdate("insert into another_table (another_col_1, another_col_2) values ('E1', 126)");
+            var id3 = Jdbi3GeneratedKeys.executeAndGenerateId(update3);
+
+            var samples = handle.createQuery("select * from another_table order by another_col_1 desc")
+                    .map(new AnotherSampleMapper())
+                    .list();
+
+            assertThat(samples).extracting("id", "anotherCol1", "anotherCol2").containsExactly(
+                    tuple(id3, "E1", 126),
+                    tuple(id2, "D1", 84),
+                    tuple(id1, "C1", 42)
+            );
+        }
+    }
+
+    @Value
+    private static class Sample {
+        Long id;
+        String col1;
+        String col2;
+    }
+
+    private static class SampleMapper implements RowMapper<Sample> {
+        @Override
+        public Sample map(ResultSet rs, StatementContext ctx) throws SQLException {
+            return new Sample(
+                    rs.getLong("id"),
+                    rs.getString("col_1"),
+                    rs.getString("col_2")
+            );
+        }
+    }
+
+    @Value
+    private static class AnotherSample {
+        Long id;
+        String anotherCol1;
+        int anotherCol2;
+    }
+
+    private static class AnotherSampleMapper implements RowMapper<AnotherSample> {
+        @Override
+        public AnotherSample map(ResultSet rs, StatementContext ctx) throws SQLException {
+            return new AnotherSample(
+                    rs.getLong("id"),
+                    rs.getString("another_col_1"),
+                    rs.getInt("another_col_2")
+            );
+        }
+    }
+
+}
