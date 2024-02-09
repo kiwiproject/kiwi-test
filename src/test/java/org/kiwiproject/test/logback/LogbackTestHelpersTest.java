7259c43c05272f6c333f11984836a1d0d13a148e
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-02-09 15:09:29 -0500

    Enhance LogbackTestHelpers
    * Make resetLogback() try logback-test.xml, then logback.xml as fallback
    * Add varargs argument to resetLogback so that it accepts a primary
      location, and optionally fallback locations

--- src/test/java/org/kiwiproject/test/logback/LogbackTestHelpersTest.java
+++ src/test/java/org/kiwiproject/test/logback/LogbackTestHelpersTest.java
@@ -5,9 +5,12 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 import ch.qos.logback.core.joran.spi.JoranException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.kiwiproject.test.junit.jupiter.ResetLogbackLoggingExtension;
+import org.kiwiproject.test.junit.jupiter.params.provider.MinimalBlankStringSource;
 
 /**
  * Unit test for {@link LogbackTestHelpers}. This mainly tests invalid arguments
@@ -18,16 +21,52 @@ import org.kiwiproject.test.junit.jupiter.ResetLogbackLoggingExtension;
 @ExtendWith(ResetLogbackLoggingExtension.class)
 class LogbackTestHelpersTest {
 
-    @Test
-    void shouldThrowIllegalArgument_WhenInvalidLogbackConfigPath() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> LogbackTestHelpers.resetLogback("dne.xml"));
+    @Nested
+    class ThrowsIllegalArgumentException {
+
+        @Test
+        void whenInvalidLogbackConfigPath() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> LogbackTestHelpers.resetLogback("dne.xml"))
+                    .withMessage("Did not find any of the Logback configurations: [dne.xml]");
+        }
+
+        @Test
+        void whenInvalidLogbackConfigPaths() {
+            assertThatIllegalArgumentException().isThrownBy(() ->
+                            LogbackTestHelpers.resetLogback("dne1.xml", "dne2.xml", "dne3.xml"))
+                    .withMessage("Did not find any of the Logback configurations: [dne1.xml, dne2.xml, dne3.xml]");
+        }
+
+        @ParameterizedTest
+        @MinimalBlankStringSource
+        void whenGivenBlankConfigLocation(String location) {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> LogbackTestHelpers.resetLogback(location))
+                    .withMessage("logbackConfigFile must not be blank");
+        }
+
+        @Test
+        void whenExplicitNullFallbackLocationsIsGiven() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> LogbackTestHelpers.resetLogback("acme-logback.xml", (String[]) null))
+                    .withMessage("fallback locations vararg parameter must not be null");
+        }
+
+        @ParameterizedTest
+        @MinimalBlankStringSource
+        void whenFallbackLocationIsBlank(String fallbackLocation) {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> LogbackTestHelpers.resetLogback("acme-test-logback.xml", fallbackLocation))
+                    .withMessage("fallbackConfigFiles must not contain blank locations");
+        }
     }
 
     @Test
     void shouldThrowUncheckedJoranException_WhenInvalidLogbackConfig() {
         assertThatExceptionOfType(UncheckedJoranException.class)
-                .isThrownBy(() -> LogbackTestHelpers.resetLogback("LogbackTestHelpersTest/invalid-logback-test.xml"))
+                .isThrownBy(() ->
+                        LogbackTestHelpers.resetLogback("LogbackTestHelpersTest/invalid-logback-test.xml"))
                 .withCauseExactlyInstanceOf(JoranException.class);
     }
 }
