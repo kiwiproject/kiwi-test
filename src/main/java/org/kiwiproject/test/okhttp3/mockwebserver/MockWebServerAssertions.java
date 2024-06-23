5aae0a284479ae7068821d7248b3f937be1b4764
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-06-23 14:39:17 -0400

    Complete javadocs on MockWebServerAssertions

--- src/main/java/org/kiwiproject/test/okhttp3/mockwebserver/MockWebServerAssertions.java
+++ src/main/java/org/kiwiproject/test/okhttp3/mockwebserver/MockWebServerAssertions.java
@@ -26,14 +26,36 @@ public class MockWebServerAssertions {
         this.mockWebServer = requireNotNull(mockWebServer);
     }
 
+    /**
+     * Starting point for fluent assertions on {@link MockWebServer}.
+     *
+     * @param mockWebServer the {@link MockWebServer} to assert upon
+     * @return this instance
+     */
     public static MockWebServerAssertions assertThat(MockWebServer mockWebServer) {
         return assertThatMockWebServer(mockWebServer);
     }
 
+    /**
+     * Starting point for fluent assertions on {@link MockWebServer}.
+     * <p>
+     * This method is provided as an alias of {@link #assertThat(MockWebServer)} to avoid conflicts
+     * when statically importing AssertJ's {@code Assertions#assertThat}, and therefore allow both
+     * to be statically imported.
+     *
+     * @param mockWebServer the {@link MockWebServer} to assert upon
+     * @return this instance
+     */
     public static MockWebServerAssertions assertThatMockWebServer(MockWebServer mockWebServer) {
         return new MockWebServerAssertions(mockWebServer);
     }
 
+    /**
+     * Asserts the {@link MockWebServer} has the expected request count.
+     *
+     * @param requestCount the expected request count
+     * @return this instance
+     */
     public MockWebServerAssertions hasRequestCount(int requestCount) {
         Assertions.assertThat(mockWebServer.getRequestCount())
                 .describedAs("Expected request count to be: %d", requestCount)
@@ -42,6 +64,13 @@ public class MockWebServerAssertions {
         return this;
     }
 
+    /**
+     * Asserts the {@link MockWebServer} has the next {@link RecordedRequest} that satisfies
+     * assertions provided by the {@code recordedRequestConsumer}.
+     *
+     * @param recordedRequestConsumer the {@link Consumer} containing the assertions on the {@link RecordedRequest}
+     * @return this instance
+     */
     public MockWebServerAssertions hasRecordedRequest(Consumer<RecordedRequest> recordedRequestConsumer) {
         var requestOrNull = RecordedRequests.takeRequestOrNull(mockWebServer);
         recordedRequestConsumer.accept(requestOrNull);
@@ -49,6 +78,13 @@ public class MockWebServerAssertions {
         return this;
     }
 
+    /**
+     * This is a terminal method that gets the next {@link RecordedRequest} (or {@code null})
+     * and makes it available as a {@link RecordedRequestAssertions} instance for further
+     * assertion chaining.
+     *
+     * @return this instance
+     */
     public RecordedRequestAssertions recordedRequest() {
         var requestOrNull = RecordedRequests.takeRequestOrNull(mockWebServer);
         return RecordedRequestAssertions.assertThatRecordedRequest(requestOrNull);
