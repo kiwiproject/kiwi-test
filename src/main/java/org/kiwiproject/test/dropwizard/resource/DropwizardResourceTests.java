8f26e2edd0b51e8f7387a0032ac1d7e3a2834b0c
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-08-19 23:29:48 -0400

    Add static modifier to  resourceBuilderPreservingLogbackConfig...
    This *might* be the cause of the delombok failures...let's make
    sure it works first, before changing those tests to use it

--- src/main/java/org/kiwiproject/test/dropwizard/resource/DropwizardResourceTests.java
+++ src/main/java/org/kiwiproject/test/dropwizard/resource/DropwizardResourceTests.java
@@ -20,7 +20,7 @@ public class DropwizardResourceTests {
      * @return a new {@link ResourceExtension.Builder} instance
      * @see ResourceExtension.Builder#bootstrapLogging(boolean)
      */
-    public ResourceExtension.Builder resourceBuilderPreservingLogbackConfig() {
+    public static ResourceExtension.Builder resourceBuilderPreservingLogbackConfig() {
         return ResourceExtension.builder().bootstrapLogging(false);
     }
 }
